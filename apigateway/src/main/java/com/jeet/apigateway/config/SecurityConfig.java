3206840c9daff6045f766b644a011edf1927e71b
jeet7122 <jeetthakkar2612@gmail.com>
2026-03-06 19:50:01 -0500

    Step 2: Configured Security configuration with SecurityFilterChain

---
+++ apigateway/src/main/java/com/jeet/apigateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.jeet.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.
+                csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**", "/api/health/**").permitAll()
+                        .requestMatchers("/api/private/**").authenticated()
+                        .anyRequest().denyAll()
+                );
+
+        return http.build();
+    }
+}
